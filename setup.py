0cb03ea32cf150d7e112495bdad2438bd9be040f
Felipe <felipe.lima@eq.ufcg.edu.br>
2019-11-13 23:04:56 -0300

    setup.py release 0.0.1

--- setup.py
+++ setup.py
@@ -1,13 +1,25 @@
 from setuptools import setup, find_packages
 
-setup(name="surropt", packages=find_packages())
+with open("README.md", 'r') as fh:
+    long_description = fh.read()
 
-# links to set a development environment so tests discovery works properly
-# http://doc.pytest.org/en/latest/goodpractices.html#install-package-with-pip
-# https://quantecon.org/wiki-py-conda-dev-env/
-
-# to install the package in development mode (in the setup.py file directory)
-# python setup.py develop
-
-# To uninstall the development package:
-# python setup.py develop --uninstall
+setup(name="surropt",
+      version='0.0.1',
+      description="Surrogate optimization toolbox for time consuming models",
+      author="Felipe Souza Lima",
+      author_email='feslima93@gmail.com',
+      url='https://github.com/feslima/surropt',
+      license='Apache License 2.0',
+      packages=find_packages(exclude=['tests_']),
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      keywords="surrogate optimization, infill criteria optimization, blackbox optimization",
+      install_requires=['numpy>=1.15', 'scipy>=1.2.0', 'pydace>=0.1.1',
+                        'pydoe2>=1.2.1'],
+      python_requires='>=3.5',
+      classifiers=[
+          "Programming Language :: Python :: 3.6",
+          "License :: OSI Approved :: Apache Software License",
+          "Operating System :: OS Independent",
+          "Topic :: Scientific/Engineering"
+      ])
